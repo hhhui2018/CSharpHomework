223b5efaacf89e2fb916f076cf6343f9c17b2b9f
hhhui2018 <43127572+hhhui2018@users.noreply.github.com>
2018-11-29 19:59:30 +0800

    Add files via upload

---
+++ homework10/EF_FirstTry/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_FirstTry
+{
+     public interface IBaseDao<T> where T : class,new()
+    {
+
+        IQueryable<T> LoadEntites();  //gain
+        T AddEntity(T entity);  //add  
+        T UpdateEntity(T entity); //update 
+        bool DelEntity(T entity); //delete
+        bool DelEntityByWhere(Func<T, bool> whereLambda); //delete according conmand
+    }
+}
---
+++ homework10/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using homework7;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        //public void UpdateOrderCustomer(uint orderId, Customer newCustomer)
+        //{
+        //    uint d = 0;
+        //    string e = textBox4.Text;
+        //    d = uint.Parse(e);
+        //    string f = textBox1.Text;
+        //}
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        uint m = 0;
+            string g = textBox4.Text;
+            m = uint.Parse(g);
+            string f = textBox1.Text;
+            DialogResult result = MessageBox.Show(
+                "OK",
+                "Title",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Asterisk);
+
+        }
+
+        private void textBox4_TextChanged()
+        {
+        
+        }
+    }
+}
